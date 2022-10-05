799d7af4227f0dafad3d85db7341f2a4c494bbc5
bluesushi <kaanturhans@gmail.com>
2022-10-05 00:33:40 -0400

    skeleton backend with default controller

---
+++ backend/src/test/java/mason/patriotmaps/PatriotmapsApplicationTests.java
@@ -0,0 +1,13 @@
+package mason.patriotmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatriotmapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
