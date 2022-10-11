5118871fe0ad0bd45e078ca817e25cbb74ebaad7
Kaan Turhan <kaanturhans@gmail.com>
2022-10-11 16:58:36 -0400

    added react code to michaels styling of login page

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -22,6 +22,7 @@ class LoginForm extends React.Component {
     }
 
     handleSubmit(event) {
+        console.log(this.state)
         fetch('localhost:8080/login', {
             method: 'POST',
             headers: {
@@ -36,13 +37,53 @@ class LoginForm extends React.Component {
     render() {
         return e(
             'form',
-            { onSubmit: this.handleSubmit, className: "d-flex flex-column align-items-center" },
-            e('h1', null, 'Log In'),
-            e('h3', null, 'Username'),
-            e('input', { onChange: this.handleUsername, value: this.state.username }),
-            e('h3', null, 'Password'),
-            e('input', { onChange: this.handlePassword, value: this.state.password, type: 'password' }),
-            e('button', { className: 'row w-25' }, 'Submit')
+            { onSubmit: this.handleSubmit },
+            // Email input
+            e(
+                'div', 
+                { className: 'form-outline mb-4' },
+                e('label', { className: 'form-label', htmlFor: 'form3Example3' }, e('font', { size: '+2' }, 'Email Address:')),
+                e('input', { type: 'email', id: 'form3Example3', className: 'form-control form-control-lg', 
+                    placeholder: 'Enter a valid email address', onChange: this.handleUsername, value: this.state.username }
+                )
+            ),
+
+            // Password input
+            e(
+                'div', 
+                { className: 'form-outline mb-3' }, 
+                e('label', { className: 'form-label', htmlFor: 'form3Example4' }, e('font', { size: '+2' }, 'Password:')),
+                e('input', { type: 'password', id: 'form3Example4', className: 'form-control form-control-lg', 
+                    placeholder: 'Enter password', onChange: this.handlePassword, value: this.state.password })
+            ),
+
+            // Checkbox container
+            e(
+                'div',
+                { className: 'd-flex justify-content-between align-items-center' },
+
+                // Checkbox
+                e(
+                    'div',
+                    { className: 'form-check mb-0' },
+                    e('input', { className: 'form-check-input me-2', type: 'checkbox', value: '', id: 'form2Example3' }),
+                    e('label', { className: 'form-check-label', htmlFor: 'form2Example3' }, 'Remember me')
+                ),
+                e('a', { href: '#!', className: 'text-body' }, 'Forgot password?')
+            ),
+
+            // Login button
+            e(
+                'div', 
+                { className: 'text-center text-lg-start mt-4 pt-2' }, 
+                e('button', { className: 'btn btn-success btn-lg', style: { paddingLeft: '2.5rem', paddingRight: '2.5rem' }}, 'Login'),
+                e(
+                    'p', 
+                    { className: 'small fw-bold mt-2 pt-1 mb-0' }, 
+                    "Don't have an account? ", 
+                    e('a', { href: '#!', className: 'link-danger' }, 'Register')
+                )
+            )
         )
     }
 }
