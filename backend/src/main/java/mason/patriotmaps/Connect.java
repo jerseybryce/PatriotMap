6e55bc17f230d11d2adaf644a0395bb989d803c0
Daseats96 <lex2703.st@gmail.com>
2022-10-20 22:21:48 -0400

    jdbc url

--- backend/src/main/java/mason/patriotmaps/Connect.java
+++ backend/src/main/java/mason/patriotmaps/Connect.java
@@ -2,33 +2,33 @@ package mason.patriotmaps;
 import java.sql.*;
 import java.util.Properties;
 public class Connect {
-        private String userName = "postgres", password = "nebvon-kujXun-4kaddi";
-        private int portNumber = 5432;
-        private String serverName = "pmap.cbo83nt50yog.us-east-1.rds.amazonaws.com";
-        private String dbms = "postgres";
+    private String userName = "postgres", password = "nebvon-kujXun-4kaddi";
+    private int portNumber = 5432;
+    private String serverName = "pmap.cbo83nt50yog.us-east-1.rds.amazonaws.com";
+    private String dbms = "postgres";
 
-        public Connection getConnection() throws SQLException {
+    public Connection getConnection() throws SQLException {
 
-            Connection conn = null;
-            Properties connectionProps = new Properties();
-            connectionProps.put("user", this.userName);
-            connectionProps.put("password", this.password);
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", this.userName);
+        connectionProps.put("password", this.password);
 
-            conn = DriverManager.getConnection(
-                    "jdbc:" + this.dbms + "://" +
-                            this.serverName +
-                            ":" + this.portNumber + "/",
-                    connectionProps);
+        conn = DriverManager.getConnection(
+                "jdbc:" + this.dbms + "://" +
+                        this.serverName +
+                        ":" + this.portNumber + "/",
+                connectionProps);
 
-            System.out.println("Connected to database");
-            return conn;
+        System.out.println("Connected to database");
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Connect().getConnection();
+        } catch (SQLException e) {
+            System.out.println(e);
         }
-
-        public static void main(String[] args) {
-            try {
-                new Connect().getConnection();
-            } catch (SQLException e) {
-                System.out.println(e);
-            }
-        }
+    }
 }
