342d88ccb9a1017142a87090bd61fd861a660f7b
Kaan Turhan <kaanturhans@gmail.com>
2022-11-01 22:04:26 -0400

    dir for html files

--- backend/src/main/java/mason/patriotmaps/controller/GreetingController.java
+++ backend/src/main/java/mason/patriotmaps/controller/GreetingController.java
@@ -6,8 +6,9 @@ import mason.patriotmaps.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
-@RestController
+@Controller
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
