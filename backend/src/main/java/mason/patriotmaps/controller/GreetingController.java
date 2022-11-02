7a0b3094f993f38b59f678220e9c05c505d90c02
Kaan Turhan <kaanturhans@gmail.com>
2022-11-01 22:24:38 -0400

    removed error dependency

--- backend/src/main/java/mason/patriotmaps/controller/GreetingController.java
+++ backend/src/main/java/mason/patriotmaps/controller/GreetingController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.Controller;
 
 @Controller
 public class GreetingController {
