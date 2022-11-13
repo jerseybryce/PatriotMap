4ad3d63f07b9610cd2b0218fef88125d6b153314
MichaelVittori <112675358+MichaelVittori@users.noreply.github.com>
2022-11-13 12:26:56 -0500

    Update MethodsController.java

--- backend/src/main/java/mason/patriotmaps/controller/MethodsController.java
+++ backend/src/main/java/mason/patriotmaps/controller/MethodsController.java
@@ -10,14 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/methods")
 public class MethodsController {
 
@@ -28,15 +29,17 @@ public class MethodsController {
     @Autowired
     ClassRepository classRepository;
 
+
     /**
      * Returns the users list of classes, or else it returns an empty ArrayList.
      * @return list of classes from the database
      */
-    @GetMapping("/getClasses")
+/*    @GetMapping("/getClasses")
+    @ResponseBody
     //first get the primary key of the class entity and then return all the classes.
     public List<String> getClassNames(){
         String username = ((UserDetails)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getUsername();
-        Collection<Integer> classes = userRepository.findByUsername(username).getClass_list();
+        List<Integer> classes = userRepository.findByUsername(username).getClass_list();
         List<String> classesAsString = new ArrayList<>();
         if(classes != null)
             for(Integer ID : classes) {
@@ -46,15 +49,20 @@ public class MethodsController {
         if(classesAsString.isEmpty()) classesAsString.add("no classes found!");
         return classesAsString;
     }
-
+*/
     /**
      * method to submit a class for the user
+     *
+     *
      */
-    @PostMapping("/enterClass")
-    public void addClass(@RequestParam ClassEntity toBeAdded){
+    @PostMapping("/addClass")
+    public String addClass(@ModelAttribute ClassEntity toBeAdded){
         String username = ((UserDetails)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getUsername();
         classRepository.save(toBeAdded);
-        userRepository.findByUsername(username).addClass(Math.toIntExact(toBeAdded.getClass_id()));
+        UserEntity user = userRepository.findByUsername(username);
+        user.addClass(Math.toIntExact(toBeAdded.getClass_id()));
+        userRepository.save(user);
+        return "redirect:/homepage";
     }
 
     /**
@@ -77,6 +85,7 @@ public class MethodsController {
      * @return latitude and longitude in this order.
      */
     @GetMapping("/getCoordinates")
+    @ResponseBody
     public long[] buildingCoordinates(long buildingID){
         Optional<BuildingEntity> building = buildingRepository.findById(buildingID);
         long[] coordinates = new long[2];
@@ -97,16 +106,16 @@ public class MethodsController {
      * @param password new Password
      */
     @PostMapping("/resetPassword")
-    public void changePassword(@RequestParam String password){
+    public void changePassword(String password){
         String username = ((UserDetails)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getUsername();
         UserEntity user = userRepository.findByUsername(username);
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(password);
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
-        userRepository.save(user);
     }
 
     @GetMapping("/get_building_aka")
+    @ResponseBody
     public List<String> getBuildingAKAS(long id){
         Optional<BuildingEntity> building = buildingRepository.findById(id);
         List<String> akas = new ArrayList<>();
