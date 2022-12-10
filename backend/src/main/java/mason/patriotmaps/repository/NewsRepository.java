122b4b14d23b966c9606c8889e6bb5066534b221
Kaan Turhan <kaanturhans@gmail.com>
2022-12-09 22:19:57 -0500

    changed some news queries

--- backend/src/main/java/mason/patriotmaps/repository/NewsRepository.java
+++ backend/src/main/java/mason/patriotmaps/repository/NewsRepository.java
@@ -3,11 +3,21 @@ package mason.patriotmaps.repository;
 import mason.patriotmaps.entity.NewsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
 
     @Query(value = "select obj_description('public.news_table'\\:\\:regclass)", nativeQuery = true)
     String getLastUpdated();
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from news_table", nativeQuery = true)
+    void clear();
+
+    @Query(value = "comment on table news_table is '?1'", nativeQuery = true)
+    void updateDay(String date);
 }
