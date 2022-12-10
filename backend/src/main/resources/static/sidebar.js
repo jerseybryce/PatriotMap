4735bae3f8e63b8725df4e87db858c5c594442ea
Michael <vittori.michael@gmail.com>
2022-12-10 11:33:54 -0500

    Removed pin

--- backend/src/main/resources/static/sidebar.js
+++ backend/src/main/resources/static/sidebar.js
@@ -4,7 +4,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-L.marker([38.8301, -77.3078]).addTo(map);
 
 //Print date
 const dateLabel = document.querySelector('.day')
